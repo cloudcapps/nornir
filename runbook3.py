8aa97fcda953f027131d79a56bd0db6b1bd077f0
Jordan Capps <jordan.capps@edf-re.com>
2022-07-09 14:57:44 -0700

    More rules

---
+++ runbook3.py
@@ -0,0 +1,12 @@
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_commands
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornir(config_file="config.yaml")
+
+def show_commands_test(task):
+    task.run(task=send_commands, commands=["show ip interface brief", "show version", "show run"])
+
+results = nr.run(task=show_commands_test)
+
+print_result(results)
